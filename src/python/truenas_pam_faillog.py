0ee2ab4cedb02eba304f6490ddc5cbd0b0c70a4f
Andrew Walker <andrew.walker@truenas.com>
2025-12-08 07:21:15 -0600

    Improve python faillog tools
    This commit improves the tools provided for interacting with the
    faillog by adding methods to do the following:
    
    * check whether an account is tally-locked on a per-user basis
    * reset tally on a per-user basis
    * generate a set of usernames that are currently tally-locked
    * optimizes function to get list of failures for a user

--- src/python/truenas_pam_faillog.py
+++ src/python/truenas_pam_faillog.py
@@ -8,15 +8,17 @@ This module provides functionality to read and iterate over PAM failure log
 entries stored in the kernel keyring by the pam_truenas module.
 """
 
+import errno
 import struct
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Iterator, Optional, Dict, Any
+from typing import Iterator, Dict, Any
 import truenas_keyring
 
 # Constants matching the C definitions
 PAM_KEYRING_NAME = "PAM_TRUENAS"
 PAM_FAILLOG_NAME = "FAILLOG"
+PAM_TALLY_LOCK_NAME = 'tally_lock'
 NAME_MAX = 255
 
 # Tally flags from tally.h
@@ -54,7 +56,7 @@ class FaillogEntry:
                 f"{self.source_type}: {self.source}")
 
 
-class FaillogIterator:
+class PamFaillog:
     """Iterator for PAM faillog entries in the kernel keyring"""
 
     def __init__(self):
@@ -161,6 +163,62 @@ class FaillogIterator:
                         failure_key.description
                     )
 
+    def __get_user_keyring(self, username: str):
+        if not self.pam_keyring:
+            self._load_keyring()
+
+        try:
+            user_keyring = self.pam_keyring.search(
+                key_type=truenas_keyring.KeyType.KEYRING,
+                description=username
+            )
+        except FileNotFoundError:
+            return None
+        except PermissionError:
+            # persistent keyring may not be initialized in current thread
+            self._load_keyring()
+            try:
+                user_keyring = self.pam_keyring.search(
+                    key_type=truenas_keyring.KeyType.KEYRING,
+                    description=username
+                )
+            except FileNotFoundError:
+                return None
+
+        return user_keyring
+
+    def __get_failure_keyring(self, user_keyring):
+        try:
+            faillog_keyring = user_keyring.search(
+                key_type=truenas_keyring.KeyType.KEYRING,
+                description=PAM_FAILLOG_NAME
+            )
+        except FileNotFoundError:
+            # truenas_pykeyring converts ENOKEY to FileNotFoundError
+            faillog_keyring = None
+
+        return faillog_keyring
+
+    def __get_lock_key(self, user_keyring):
+        try:
+            lock_key = user_keyring.search(
+                key_type=truenas_keyring.KeyType.USER,
+                description=PAM_TALLY_LOCK_NAME
+            )
+        except FileNotFoundError:
+            # truenas_pykeyring converts ENOKEY to FileNotFoundError
+            return None
+        except truenas_keyring.KeyringError as exc:
+            if exc.errno == errno.EKEYEXPIRED or exc.errno == errno.EKEYREVOKED:
+                # Forcing cpython extension iterate keyring contents forces deletion
+                # of expired lock
+                user_keyring.list_keyring_contents()
+                return None
+
+            raise
+
+        return lock_key
+
     def get_user_failures(self, username: str) -> list[FaillogEntry]:
         """
         Get all failure entries for a specific user
@@ -172,9 +230,20 @@ class FaillogIterator:
             List of FaillogEntry objects for the user
         """
         entries = []
-        for entry in self.iterate():
-            if entry.username == username:
-                entries.append(entry)
+        if (user_keyring := self.__get_user_keyring(username)) is None:
+            return entries
+
+        if (failures := self.__get_failure_keyring(user_keyring)) is None:
+            return entries
+
+        for fail_entry in failures.iter_keyring_contents(
+            unlink_revoked=True,
+            unlink_expired=True
+        ):
+            key_data = fail_entry.read_data()
+            entry = self.parse_entry(key_data, username, fail_entry.description)
+            entries.append(entry)
+
         return entries
 
     def get_statistics(self) -> Dict[str, Any]:
@@ -216,3 +285,60 @@ class FaillogIterator:
                 stats['locked_users'].append(username)
 
         return stats
+
+    def is_tally_locked(self, username: str) -> bool:
+        """ This function checks for whether the pam_truenas user keyring has a tally_lock key set on it. """
+        if (user_keyring := self.__get_user_keyring(username)) is None:
+            # No user keyring / session info. It's not possible for it to be tally locked.
+            return False
+
+        lock_key = self.__get_lock_key(user_keyring)
+        return lock_key is not None
+
+    def reset_tally(self, username: str) -> None:
+        """ Reset FAILLOG for the specified username and remove its tally-lock """
+        if (user_keyring := self.__get_user_keyring(username)) is None:
+            # No user keyring / session info. It's not possible for it to be tally locked.
+            return None
+
+        if (failure_keyring := self.__get_failure_keyring(user_keyring)) is not None:
+            # Wipe all failed login attempts from log
+            failure_keyring.clear()
+
+        # There is a separate key thats presence indicates the user is
+        # tally locked. Remove this as well if it exists.
+        if (lock_key := self.__get_lock_key(user_keyring)) is None:
+            return
+
+        # Unlink the locking key from the user keyring.
+        user_keyring.unlink_key(lock_key.serial)
+
+    def tally_locked_users(self) -> set[str]:
+        """ Create a set of usernames of all users who are currently tally locked. The
+        TRUENAS_PAM_KEYRING key descriptions contain the affected user's name. """
+        locked_users = set()
+        if not self.pam_keyring:
+            self._load_keyring()
+
+        try:
+            pam_iter = self.pam_keyring.iter_keyring_contents(
+                unlink_revoked=True,
+                unlink_expired=True
+            )
+        except PermissionError:
+            self._load_keyring()
+            pam_iter = self.pam_keyring.iter_keyring_contents(
+                unlink_revoked=True,
+                unlink_expired=True
+            )
+
+        for user_keyring in pam_iter:
+            try:
+                if self.__get_lock_key(user_keyring):
+                    locked_users.add(user_keyring.key.description)
+            except Exception:
+                # This is called in user.query extend context. We can't allow exceptions or logs to be raised here
+                # because the code path is really hot.
+                pass
+
+        return locked_users
